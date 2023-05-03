4f387857576fc64eee08895b5aec409340682057
Akas_Datta <012akasdatta@gmail.com>
2023-05-04 03:59:00 +0600

    modified json data

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const categories = require('./data/categories.json')
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Dragon is running')
 })
 
 app.get('/categories', (req, res) => {
+    // console.log(categories)
     res.send(categories)
 })
+
+app.get('/categories/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id)
+    const selectedCatagories = categories.find(n => n.id === id);
+    res.send(selectedCatagories);
+})
+
+
 app.listen(port, () => {
     console.log(`Dragon API is running on port: ${port}`)
 })
